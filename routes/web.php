bdd750dc72bd4d604adc70475c5af4dde1b42c48
Your Name silvianadevi1109 <silvianadevi1109@gmail.com>
2022-04-24 02:05:48 +0700

    commit6

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CobaController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,7 +14,24 @@ use App\Http\Controllers\CobaController;
 |
 */
 
+/*Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/coba', function () {
+    return view('coba');
+});
+Route::get('/coba/{no}', [CobaController::class, 'coba']); 
+Route::get('/test', [CobaController::class, 'index']); 
+Route::get('/test/{ke}', [CobaController::class, 'urutan']);
+
+*/
+
+Route::get('', [CobaController::class, 'index']);
 Route::get('/friends', [CobaController::class, 'index']);
 Route::get('/friends/create', [CobaController::class, 'create']);
 Route::post('/friends', [CobaController::class, 'store']);
+Route::get('/friends/{id}', [CobaController::class, 'show']);
+Route::get('/friends/{id}/edit', [CobaController::class, 'edit']);
+Route::put('/friends/{id}', [CobaController::class, 'update']);
+Route::delete('/friends/{id}', [CobaController::class, 'destroy']);
